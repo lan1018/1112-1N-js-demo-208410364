250bcf11621c87093a9791fa54401a1093735eae
frankielan168 <frankielan168@gmail.com>
2023-03-16 20:11:33 +0800

    W05-P2: implement all filter buttons with all tests in console.log

--- app_64.js
+++ app_64.js
@@ -3,7 +3,6 @@ import { menu } from './data.js';
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('.btn-container');
 
-
 const displayMenuItems = (menu) => {
     let displayMenu = menu.map((item) => {
         return `
@@ -27,6 +26,7 @@ const displayMenuItems = (menu) => {
     sectionCenter.innerHTML = displayMenu;
 }
 
+
 const categories = ['all', 'breakfast', 'lunch', 'dinner', 'dessert', 'shakes'];
 // const categories = ['all', ...new Set(menu.map((item) => item.category))];
 
@@ -34,15 +34,37 @@ const displayMenuButtons = () => {
   let menuButtons = categories.map(category => {
     return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`;
   })
-  console.log('displayButtons before join', menuButtons);
+  // console.log('displayButtons before join', menuButtons);
   menuButtons = menuButtons.join('');
-  console.log('displayButtons after join/n', menuButtons);
+  // console.log('displayButtons after join/n', menuButtons);
   btnContainer.innerHTML = menuButtons;
+
+  const filterBtns = document.querySelectorAll(
+    '.filter-btn');
+  console.log('filterbtns',filterBtns);
+  filterBtns.forEach( (btn) => {
+    btn.addEventListener("click", (e) => {
+      console.log('data-id', e.currentTarget.
+      dataset.id);
+      const category = e.currentTarget.dataset.
+      id;
+      const filterMenu = menu.filter( (item) => 
+      item.category === category);
+
+      if(category === 'all'){
+        console.log('all', menu);
+        displayMenuItems(menu);
+      }else{
+        console.log(category, filterMenu);
+        displayMenuItems(filterMenu);
+      }
+    });
+  });
 }
 
 
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
-    displayMenuButtons();
+    displayMenuButtons(menu);
 });
 

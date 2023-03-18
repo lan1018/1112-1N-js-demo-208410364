c2f4dc1bd735fbed61923ca5fc9c7c85a74df0dd
lan1018 <frankielan168@gmail.com>
2023-03-18 11:33:56 +0800

    W05-P5:  press 3 different buttons can show 3 different modals

--- demo/W05-modal-starter/script_64.js
+++ demo/W05-modal-starter/script_64.js
@@ -1,26 +1,33 @@
-const modal = document.querySelector('.modal');
+const modals = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log('modal', modal);
-console.log('show-modal', btnsOpenModal);
+// console.log('modal', modals);
+// console.log('show-modal', btnsOpenModal);
 
-const openModal = () => {
+const openModal = (e) => {
+    const modalId = e.target.getAttribute('data-modal-id');
+    const modal = document.getElementById(modalId);
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
 
 const closeModal = () => {
-    modal.classList.add('hidden');
+    for(let i = 0; i < modals.length; i++) {
+        modals[i].classList.add('hidden');
+    }
     overlay.classList.add('hidden');
 };
 
 for(let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener('click', openModal);
-}
+  }
 
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    if (e.key === 'Escape' && !modals.classList.contains('hidden')) {
       closeModal();
     }
   });
+
+  overlay.addEventListener('click', closeModal);

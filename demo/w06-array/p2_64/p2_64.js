d46ac6e2b3fffd73608fb41a1b87f36d1a3e5d55
frankielan168 <frankielan168@gmail.com>
2023-03-23 20:34:36 +0800

    W06-P3: import students and sdata array and do sorting, find the highest and lowest score

--- demo/w06-array/p2_64/p2_64.js
+++ demo/w06-array/p2_64/p2_64.js
@@ -1,18 +1,32 @@
 import { students, sdata } from './data_64.js';
 
+// for sdata
+
 console.log("sdata original", sdata);
 
-const students2 = students.map((student) => {
-    student.role = "student";
-    return student;
-});
-
-console.log("students2", students2);
-
 sdata.sort( function(a,b){return b-a});
-console.log("sdata", sdata);
+console.log("sdata sorted", sdata);
 console.log(`The hightest score: ${sdata[0]}`);
 console.log(`The lowest score: ${sdata[sdata.length-1]}`);
 
-students.sort(function(a,b){return b.score - a.score })
+
+// for students
+
 console.log("students", students);
+
+const students2 = students.map((student) => {
+    // student.role = "student";
+    return {...student, role: "student"};
+});
+
+console.log("students2 original", students2);
+
+students2.sort(function(a,b){return a.score - b.score });
+console.log("student2 sorted", students2);
+
+console.log(`The lowest score: ${students2[0].score}`);
+console.log(`The hightest score: ${students2[students2.length-1].score}`);
+
+
+
+

06e66d7aa4e17ef151b638794d4199438bf0b924
lan1018 <frankielan168@gmail.com>
2023-04-14 21:40:59 +0800

    modify class .banner-section

--- js/classdemo_64.js
+++ js/classdemo_64.js
@@ -2,7 +2,7 @@ const showClassDemo = (week) => {
     const showDemo = document.querySelector('.banner-section');
     switch(week){
         case "w1":
-            showDemo.innerHTML = `<iframe src='./demo/Wo1/index.html' width="100%" height="100%" />`; 
+            showDemo.innerHTML = `<iframe src='./demo/W01/index.html' width="100%" height="100%" />`; 
             break;
         case "w2":
 

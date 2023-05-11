74df86dfc8f4b52d94c9336bacd99ad869c2dfe7
frankielan168 <frankielan168@gmail.com>
2023-05-11 20:24:02 +0800

    W13-P2: Show /api/data.json, img using remote_url

---
+++ demo/W13-menu-api/app_64.js
@@ -0,0 +1,83 @@
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
+
+// import { menu } from './data.js';
+const url = './api/data.json';
+
+let menu;
+
+const displayMenuItems = (menu) => {
+    let displayMenu = menu.map((item) => {
+        return `
+        <article class="menu-item">
+        <img src=${item.remote_img} alt=${item.title} class="photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+          </header>
+          <p class="item-text">
+            ${item.descrip}
+          </p>
+        </div>
+      </article>
+      `
+    });
+    // console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    // console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+
+// const categories = ['all', 'breakfast', 'lunch', 'dinner', 'dessert', 'shakes'];
+
+const displayMenuButtons = () => {
+  const categories = ['all', ...new Set(menu.map((item) => item.category))];
+  let menuButtons = categories.map(category => {
+    return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`;
+  })
+  // console.log('displayButtons before join', menuButtons);
+  menuButtons = menuButtons.join('');
+  // console.log('displayButtons after join/n', menuButtons);
+  btnContainer.innerHTML = menuButtons;
+
+  const filterBtns = document.querySelectorAll(
+    '.filter-btn');
+  console.log('filterbtns',filterBtns);
+  filterBtns.forEach( (btn) => {
+    btn.addEventListener("click", (e) => {
+      console.log('data-id', e.currentTarget.
+      dataset.id);
+      const category = e.currentTarget.dataset.
+      id;
+      const filterMenu = menu.filter( (item) => 
+      item.category === category);
+
+      if(category === 'all'){
+        console.log('all', menu);
+        displayMenuItems(menu);
+      }else{
+        console.log(category, filterMenu);
+        displayMenuItems(filterMenu);
+      }
+    });
+  });
+}
+
+const fetchData = async () => {
+  try{
+    const response = await fetch(url);
+    const data = response.json();
+    console.log('fetchData',fetchData);
+    return data;
+} catch(error){
+    console.log(error);
+}
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  menu = await fetchData();
+  await displayMenuItems(menu);
+  await displayMenuButtons();
+});
+
---
+++ demo/W13-menu-api/data.js
@@ -0,0 +1,56 @@
+export const menu = [
+    {
+        id: 1,
+        title: 'egg',
+        category: 'breackfast',
+        price: 6.56,
+        img: './menu/images/my-1.jpg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    },
+    {
+        id: 2,
+        title: 'green eating',
+        category: 'lunch',
+        price: 15.55,
+        img: './menu/images/my-2.jpg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    },
+    {
+        id: 3,
+        title: 'meat balls with noodles',
+        category: 'dinner',
+        price: 27.89,
+        img: './menu/images/my-3.jpg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    },
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'dessert',
+        price: 15.99,
+        img: './menu/images/item-1.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    },
+    {
+        id: 5,
+        title: 'dinner double',
+        category: 'dinner',
+        price: 13.99,
+        img: './menu/images/item-2.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    },
+    {
+        id: 6,
+        title: 'gozilla milkshake',
+        category: 'shakes',
+        price: 6.99,
+        img: './menu/images/item-3.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo minima asperiores! Neque iste, accusantium inventore doloribus minus voluptatibus reprehenderit qui rem veniam soluta aspernatur magnam obcaecati! Sit, error laudantium.'
+    }
+];
